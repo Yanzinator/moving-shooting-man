e81c57e651976c4e0f936a8ee9e421cea9d3459a
yannzin <hanzinyankee4@gmail.com>
2021-11-14 17:44:25 -0500

    movement
    character can now move forward. also added rotation speed variable

--- Assets/character.cs
+++ Assets/character.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class character : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
-    float m_Speed;
+    public float m_Speed;
+    public float r_Speed;
 
     void Start()
     {
         //Fetch the Rigidbody component you attach from your GameObject
         m_Rigidbody = GetComponent<Rigidbody>();
         //Set the speed of the GameObject
-        m_Speed = 10.0f;
+        m_Speed = 3.0f;
+        r_Speed = 10.0f;
     }
 
     void Update()
@@ -20,25 +22,25 @@ public class character : MonoBehaviour
         if (Input.GetKey("w"))
         {
             //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
-            m_Rigidbody.velocity = transform.forward * m_Speed;
+            transform.position += transform.up * Time.deltaTime * m_Speed;
         }
 
         if (Input.GetKey("s"))
         {
             //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
-            m_Rigidbody.velocity = -transform.forward * m_Speed;
+            transform.position -= transform.up * Time.deltaTime * m_Speed;
         }
 
         if (Input.GetKey("a"))
         {
             //Rotate the sprite about the Y axis in the positive direction
-            transform.Rotate(new Vector3(0, 0, 1) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(0, 0, 5) * Time.deltaTime * r_Speed, Space.World);
         }
 
         if (Input.GetKey("d"))
         {
             //Rotate the sprite about the Y axis in the negative direction
-            transform.Rotate(new Vector3(0, 0, -1) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(0, 0, -5) * Time.deltaTime * r_Speed, Space.World);
         }
     }
 }
