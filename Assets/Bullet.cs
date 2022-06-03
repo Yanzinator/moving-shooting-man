88ce368db0527b2f862424fa4911cace45b121c3
yannzin <hanzinyankee4@gmail.com>
2022-06-03 09:15:19 -0500

    added bullet class and use for character
    added a class and object called bullet  that can be used to shoot bullets by anything. also added ability to shoot using bullet class to character

---
+++ Assets/Bullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet
+{
+    public float xPosition;
+    public float yPosition;
+    public float Rotation;
+    public float Velocity;
+    public Sprite sprite;
+
+
+
+    // should be called once all variables are decided
+    public void Create()
+    {
+
+        GameObject Bullet = new GameObject("Empty");
+
+        SpriteRenderer spriteRenderer = Bullet.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        BulletBehavior script = Bullet.AddComponent(typeof(BulletBehavior)) as BulletBehavior;
+        script.speed = Velocity;
+
+        spriteRenderer.sprite = sprite;
+
+        Bullet.transform.position = new Vector2(xPosition,yPosition);
+        Bullet.transform.Rotate(0,0,Rotation);
+        Bullet.transform.localScale = new Vector2(3, 3);
+    }
+}
--- Assets/character.cs
+++ Assets/character.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Bullet;
 
 public class character : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
     public float movementSpeed;
     public float rotationSpeed;
+    public float shootingSpeed;
+    public float bulletVelocity;
+    public Sprite BulletSpr;
+
 
     void Start()
     {
@@ -43,9 +48,29 @@ public class character : MonoBehaviour
             transform.Rotate(new Vector3(0, 0, -5) * Time.deltaTime * rotationSpeed, Space.World);
         }
     }
+
+    float cooldown = 0;
+
+    void Shoot()
+    {
+        cooldown += Time.deltaTime;
+        if (Input.GetKey("space") && cooldown >= shootingSpeed)
+        {
+            
+            Bullet bullet = new Bullet();
+            bullet.sprite = BulletSpr;
+            bullet.xPosition = transform.position.x;
+            bullet.yPosition = transform.position.y;
+            bullet.Rotation = transform.eulerAngles.z;
+            bullet.Velocity = bulletVelocity;
+            bullet.Create();
+            cooldown = 0;
+        }
+    }
     
     void Update()
     {
         Move();
+        Shoot();
     }
 }
